96d4631648d85e41b7d0e864d9ab90bd949b23d3
olafkry208 <olafkry208@student.polsl.pl>
2019-06-20 00:09:54 +0200

    Extend stub

--- src/Language.php
+++ src/Language.php
@@ -5,5 +5,45 @@ namespace Kryus\GeneRally;
 
 class Language
 {
-    //TODO
+    /** @var string[] */
+    private $translations;
+
+    /**
+     * @param string[] $translations
+     */
+    public function __construct(array $translations)
+    {
+        $this->translations = $translations;
+    }
+
+    /**
+     * @param string $filename
+     * @return Language
+     */
+    public static function createFromFile(string $filename): Language
+    {
+        $file = file_get_contents($filename);
+        $translations = [];
+
+        //TODO
+
+        return new self($translations);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getTranslations(): array
+    {
+        return $this->translations;
+    }
+
+    /**
+     * @param int $id
+     * @return string|null
+     */
+    public function getTranslation(int $id): ?string
+    {
+        return $this->translations[$id] ?? null;
+    }
 }
